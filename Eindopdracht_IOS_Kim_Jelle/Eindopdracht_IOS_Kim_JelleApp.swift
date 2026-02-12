726644fcc50f84a7e269dd370472e6112bcf1fb8
jjpreuser <jjp.reuser@student.avans.nl>
2026-02-12 11:10:22 +0100

    Initial Commit

---
+++ Eindopdracht_IOS_Kim_Jelle/Eindopdracht_IOS_Kim_JelleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eindopdracht_IOS_Kim_JelleApp.swift
+//  Eindopdracht_IOS_Kim_Jelle
+//
+//  Created by Jelle Reuser on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Eindopdracht_IOS_Kim_JelleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
